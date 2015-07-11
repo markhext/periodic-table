b9ac9b2593a5a2791c86e997ff78d0d8e0ab723b
Mark Hext <hextcode@gmail.com>
2015-07-11 23:24:00 +0100

    First-deployment

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-console.log(path.join(__dirname + '/public/index.html'));
+
+var port = process.env.PORT || 3000;
+
+
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.use("/public", express.static(path.join(__dirname, 'public')));
-
-app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
